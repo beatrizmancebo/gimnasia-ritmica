f859b53f1846f3b746a099ef355f9b41f0587c7f
Beatriz <Beatrizmancebom@gmail.com>
2019-12-27 15:37:53 +0100

    cambio8

--- js/functions.js
+++ js/functions.js
@@ -2,9 +2,9 @@
     var edad = prompt("¿Cuantos años tienes?");
     var fecha = new Date();
     if (fecha.getHours() < 12) {
-        document.getElementById("saludo").innerHTML = ' Buenos días ' + nombre + ' . Disfruta de tus ' + edad + ' años.';
+        document.getElementById("saludo").innerHTML = ' Buenos días ' + nombre + ' . Disfruta de la presentación'
     } else {
-        document.getElementById("saludo").innerHTML = ' Buenos tardes ' + nombre + ' . Disfruta de tus ' + edad + ' años.';
+        document.getElementById("saludo").innerHTML = ' Buenos tardes ' + nombre + ' . Disfruta de la presentación'
     }
 
 alert("PINCHA EN EL LOGO DE INSTAGRAM")
